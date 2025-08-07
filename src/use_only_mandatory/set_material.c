166ee2ac4a164fa58db6ba8a8f570f4f24480e46
42kharuya <haruya.0411.k@gmail.com>
2025-08-07 21:04:08 +0900

    fix: update sample scene files and improve material setting logic

--- src/use_only_mandatory/set_material.c
+++ src/use_only_mandatory/set_material.c
@@ -6,7 +6,7 @@
 /*   By: kharuya <haruya.0411.k@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 08:02:33 by kharuya           #+#    #+#             */
-/*   Updated: 2025/08/02 18:00:08 by kharuya          ###   ########.fr       */
+/*   Updated: 2025/08/05 04:17:13 by kharuya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 t_binary_result	set_material(t_obj *obj, char **line_element, int start_index)
 {
-	return (set_material_common(obj, line_element, start_index));
+	if (set_color(&(obj->material.color), line_element[start_index]) == FAILURE)
+		return (put_out_format_error(line_element[0], ERR_INVALID_VALUE));
+	if (!line_element[start_index + 1])
+		return (set_material_default(obj));
+	else
+	{
+		if (set_material_common(obj, line_element, start_index) == FAILURE)
+			return (FAILURE);
+	}
+	return (SUCCESS);
 }
